258a981614bd3ef496a64da34d6a3e09ed362e2e
Chris Peak <cpeak@Chriss-MacBook-Pro.local>
2014-04-09 22:31:47 -0500

    Updates and test commit from new mbp

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,8 +2,10 @@ class Note < ActiveRecord::Base
   attr_accessible :content, :person_id
   belongs_to :person
 
-  def fullName 
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
+  def fullName
     (Person.find person_id).fullName
   end
-
 end
